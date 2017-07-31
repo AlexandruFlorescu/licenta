b1bf39e713cfc8c625bcaf7b59c89551996011ed
FlowerDts <acdc152@gmail.com>
2017-07-31 10:31:09 +0300

    table display. Much better looking :3

--- components/CrewProfileContainer.js
+++ components/CrewProfileContainer.js
@@ -15,12 +15,12 @@ import Reputation from '../assets/carma.svg';
 
 const Img = styled.img`
   background-color: red;
-  width: 250px;
-  height: 250px;
+  width: 130px;
+  height: 130px;
   clip-path: circle(50% at center);
   shape-outside: circle(50%);
-  box-shadow: inset 0px 0px 5px 2px ${props=> props.theme.color}, inset -7px -2px 20px 10px rgba(255,255,255,.4);
   border-radius:50%;
+  box-shadow: inset 0px 0px 5px 2px ${props=> props.theme.color}, inset -7px -2px 20px 10px rgba(255,255,255,.4);
   display: inline;
   `;
 
@@ -36,7 +36,7 @@ const TopWrapper = styled.div`
 const BottomWrapper = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-end;
   width: 100%;
   height:50%;
   padding: 5px;
@@ -50,13 +50,11 @@ const Divider = styled.div`
 
 const SImg = styled.img`
   background-color: red;
-  width: 20px;
-  height: 20px;
-  clip-path: circle(50% at center);
-  shape-outside: circle(50%);
-  border-radius:50%;
-  display: float;
+  width: 22px;
+  height: 22px;
+  float: left;
   margin-right: 5px;
+  margin-top: 5px;
   `;
 
 const Header = styled.h2`
@@ -67,16 +65,42 @@ const Header = styled.h2`
 
 const Column = styled.ul`
   width: 50%;
-  height: 100%;
+  height: auto;
+  &:nth-child(1){
+    border-right: 2px solid ${props=>props.theme.color};
+  }
   `;
 
 const Block = styled.li`
   display: inline-block;
   width: 100%;
   height: 15%;
-  border: 1px solid ${props=>props.theme.color};
+  &:nth-child(1){
+    border-bottom: 2px solid ${props=>props.theme.color};
+  }
 `;
 
+const Table = styled.table`
+width: 100%;
+height: 100%;
+`;
+
+const Tr = styled.tr`
+  &:nth-child(even){
+    background: ${props=>lighten(0.4,props.theme.color)};
+  }
+`;
+
+const Th = styled.th`
+  background: ${props=>props.theme.color};
+  border: 1px solid black;
+`;
+
+const Td = styled.td`
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid ${props=>props.theme.color};
+`;
 
 class CrewProfileContainer extends Component{
   constructor(props){
@@ -98,40 +122,43 @@ class CrewProfileContainer extends Component{
         </TopWrapper>
         <Divider></Divider>
         <BottomWrapper>
-          <Column>
-            <Block>
-              <SImg></SImg> <Header>Role: CrewMemberRole</Header> <br/>
-                            <Header> CrewmemberName</Header> <br/>
-            </Block>
-            <Block>
-              <SImg></SImg> <Header>Role: CrewMemberRole</Header> <br/>
-                            <Header> CrewmemberName</Header> <br/>
-            </Block>
-            <Block>
-              <SImg></SImg> <Header>Role: CrewMemberRole</Header> <br/>
-                            <Header> CrewmemberName</Header> <br/>
-            </Block>
-            <Block>
-              <SImg></SImg> <Header>Role: CrewMemberRole</Header> <br/>
-                            <Header> CrewmemberName</Header> <br/>
-            </Block>
-            <Block>
-              <SImg></SImg> <Header>Role: CrewMemberRole</Header> <br/>
-                            <Header> CrewmemberName</Header> <br/>
-            </Block>
-          </Column>
-          <Column>
-            <SImg></SImg> <Header>Role: CrewMemberRole</Header> <br/>
-                          <Header> CrewmemberName</Header> <br/>
-            <SImg></SImg> <Header>Role: CrewMemberRole</Header> <br/>
-                          <Header> CrewmemberName</Header> <br/>
-            <SImg></SImg> <Header>Role: CrewMemberRole</Header> <br/>
-                          <Header> CrewmemberName</Header> <br/>
-            <SImg></SImg> <Header>Role: CrewMemberRole</Header> <br/>
-                          <Header> CrewmemberName</Header> <br/>
-            <SImg></SImg> <Header>Role: CrewMemberRole</Header> <br/>
-                          <Header> CrewmemberName</Header> <br/>
-          </Column>
+            <Table>
+              <Tr>
+                <Th>Role</Th>
+                <Th>Name</Th>
+                <Th>Tools</Th>
+              </Tr>
+              <Tr>
+                <Td>Captain</Td>
+                <Td>Alexandru Florescu</Td>
+                <Td>HTML5</Td>
+              </Tr>
+              <Tr>
+                <Td>Captain</Td>
+                <Td>Alexandru Florescu</Td>
+                <Td>HTML5</Td>
+              </Tr>
+              <Tr>
+                <Td>Captain</Td>
+                <Td>Alexandru Florescu</Td>
+                <Td>HTML5</Td>
+              </Tr>
+              <Tr>
+                <Td>Captain</Td>
+                <Td>Alexandru Florescu</Td>
+                <Td>HTML5</Td>
+              </Tr>
+              <Tr>
+                <Td>Captain</Td>
+                <Td>Alexandru Florescu</Td>
+                <Td>HTML5</Td>
+              </Tr>
+              <Tr>
+                <Td>Captain</Td>
+                <Td>Alexandru Florescu</Td>
+                <Td>HTML5</Td>
+              </Tr>
+            </Table>
         </BottomWrapper>
       </StrippedContainer>
     )
